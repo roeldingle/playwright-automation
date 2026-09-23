3257bb88cf6bff477d47a8b1d7adc6f0facb4c4d
roeldingle <roeldingle@gmail.com>
2026-09-24 04:52:23 +0800

    Uncomment projects-browser options

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,19 +57,18 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
       }
     },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      }
+    },
 
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   }
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   }
-    // },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      }
+    },
   ],
 });
